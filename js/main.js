3d5ab495bc295ce7c8ed857825f81b291b7d1600
NicolasHenryCPNV <nicolas.henry@cpnv.ch>
2018-03-30 15:28:31 +0200

    Delete correction, add exercice

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 *
 * Cours RIA1
 * Crée par Nicolas Henry
-* Dérnière modifications: 29.03.2018
+* Dérnière modifications: 30.03.2018
 *
 */
 
@@ -10,54 +10,8 @@
 $( document ).ready( function () {
 
   //Start plugin 
-  $.fn.slide = function () {
-    //Show the first and hide other in css
-    $( '#slider img' ).first().addClass('display');
-    //Count number of image
-    $( "#slider img" ).each( function( index ) {
-      $( this ).attr('id',index);
-      let i = 0;
-      //Generate button for images
-      while(i != index)
-        i++;i++;
-        $( '.slides' ).append(`<button id="${i}" class="btn-primary btn"> image ${i} </button>`);
-
-    });
-
-    $( "button" ).click( function () {
-
-      //disable button on click
-      $( 'button' ).prop('disabled', false);
-      $( this ).prop('disabled', true);
-      $imgId = this.id-1;
-      imgData = `img#${$imgId}`;
-      $( 'img' ).fadeOut(1000, "linear");
-      $( 'img' ).removeClass('display');
-      $( imgData ).fadeIn(1000, "linear");
-      $( imgData ).addClass('display');
-
-    });
-
-    //Change image on time
-    setInterval( function () {
-
-      $maxSwitch = $( 'img' ).length;
-      $( "img.display" ).fadeOut( function () {
-        $( 'img' ).removeClass('display');
-        let $current = $( this).prop('id');
-        $current++;
-        $( `img#${$current}` ).fadeIn(1000, "linear");
-        $( `img#${$current}` ).addClass('display');
-
-        if ( $maxSwitch == $current ) {
-          $current = 0;
-          $( `img#${$current}` ).fadeIn(1000, "linear");
-          $( `img#${$current}` ).addClass('display');
-        }
-
-      });
-    }, 5000);    
-
+  $.fn.slide = function () {   
+    //Do something here
   };
 
   //Plugin use
